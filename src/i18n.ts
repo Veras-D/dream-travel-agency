3559e61364d7eff57f0ad072d217e1d28b9ee858
Veras-D <dveras2310@gmail.com>
2025-09-21 17:02:54 -0300

    feat(i18n): Update i18n config file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+import en from './locales/en.json';
+import pt from './locales/pt.json';
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -12,6 +12,14 @@ i18n
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: en.translation,
+      },
+      pt: {
+        translation: pt.translation,
+      },
     },
   });
 
